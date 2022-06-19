c19a8714a265a2bb58062f5d7c5bd95097886488
Tom Yuval <tomuvak@gmail.com>
2022-06-19 19:00:22 +0200

    Add extension (infix) methods Optional<>.or,orMaybe – with overloads taking a given value or a computation (corresponding to the Elvis operator of nullables, with .or serving for defaults of the underlying type and .orMaybe for defaults which are themselves Optional)

--- src/commonMain/kotlin/Optional.kt
+++ src/commonMain/kotlin/Optional.kt
@@ -1,5 +1,6 @@
 package com.tomuvak.optional
 
+import com.tomuvak.optional.Optional.None
 import com.tomuvak.optional.Optional.Value
 
 sealed class Optional<out T> {
@@ -8,3 +9,23 @@ sealed class Optional<out T> {
 }
 
 val <T> Optional<T>.forcedValue: T get() = (this as Value).value
+
+infix fun <T> Optional<T>.or(default: T): T = when(this) {
+    None -> default
+    is Value -> value
+}
+
+infix fun <T> Optional<T>.or(defaultProvider: () -> T): T = when(this) {
+    None -> defaultProvider()
+    is Value -> value
+}
+
+infix fun <T> Optional<T>.orMaybe(default: Optional<T>): Optional<T> = when(this) {
+    None -> default
+    is Value -> this
+}
+
+infix fun <T> Optional<T>.orMaybe(defaultProvider: () -> Optional<T>): Optional<T> = when(this) {
+    None -> defaultProvider()
+    is Value -> this
+}
--- src/commonTest/kotlin/OptionalTest.kt
+++ src/commonTest/kotlin/OptionalTest.kt
@@ -2,11 +2,31 @@ package com.tomuvak.optional
 
 import com.tomuvak.optional.Optional.None
 import com.tomuvak.optional.Optional.Value
+import com.tomuvak.optional.test.assertNone
+import com.tomuvak.optional.test.assertValue
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFails
+import kotlin.test.fail
 
 class OptionalTest {
     @Test fun failsToForceNoneValue() { assertFails { None.forcedValue } }
     @Test fun forcesValue() = assertEquals(3, Value(3).forcedValue)
+
+    @Test fun replacesNoneWithDefault() = assertEquals(5, None or 5)
+    @Test fun doesNotReplaceValueWithDefault() = assertEquals(3, Value(3) or 5)
+
+    @Test fun replacesNoneWithComputedDefault() = assertEquals(5, None or { 5 })
+    @Test fun doesNotComputeDefaultWhenValue() =
+        assertEquals(3, Value(3) or { fail("Not supposed to be called") })
+
+    @Test fun replacesNoneWithDefaultNone() = assertNone(None orMaybe None)
+    @Test fun replacesNoneWithDefaultValue() = assertValue(5, None orMaybe Value(5))
+    @Test fun doesNotReplaceValueWithDefaultNone() = assertValue(3, Value(3) orMaybe None)
+    @Test fun doesNotReplaceValueWithDefaultValue() = assertValue(3, Value(3) orMaybe Value(5))
+
+    @Test fun replacesNoneWithComputedDefaultNone() = assertNone(None orMaybe { None })
+    @Test fun replacesNoneWithComputedDefaultValue() = assertValue(5, None orMaybe { Value(5) })
+    @Test fun doesNotComputeDefaultOptionalWhenValue() =
+        assertValue(3, Value(3) orMaybe { fail("Not supposed to be called") })
 }
