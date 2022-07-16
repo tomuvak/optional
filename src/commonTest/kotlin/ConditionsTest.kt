941876a819b2dd4f22176a6f56ba2b9cbe2d2ddd
Tom Yuval <tomuvak@gmail.com>
2022-07-16 23:14:40 +0200

    ConditionsTest (refactor): simplify using scriptedProvider

--- src/commonTest/kotlin/ConditionsTest.kt
+++ src/commonTest/kotlin/ConditionsTest.kt
@@ -4,22 +4,15 @@ import com.tomuvak.optional.test.assertNone
 import com.tomuvak.optional.test.assertValue
 import com.tomuvak.testing.assertions.mootProvider
 import com.tomuvak.testing.assertions.scriptedFunction
+import com.tomuvak.testing.assertions.scriptedProvider
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ConditionsTest {
     @Test fun thenYieldsNoneWhenFalse() = assertNone(false.then(3))
     @Test fun thenYieldsValueWhenTrue() = assertValue(3, true.then(3))
 
     @Test fun thenWithProviderWhenFalse() = assertNone(false.then(mootProvider))
-    @Test fun thenWithProviderWhenTrue() {
-        var numCalls = 0
-        assertValue(3, true.then {
-            numCalls++
-            3
-        })
-        assertEquals(1, numCalls)
-    }
+    @Test fun thenWithProviderWhenTrue() = assertValue(3, true.then(scriptedProvider(3)))
 
     @Test fun ifSatisfiesWhenDoesNotSatisfy() = assertNone(3.ifSatisfies(scriptedFunction(3 to false)))
     @Test fun ifSatisfiesWhenSatisfies() = assertValue(3, 3.ifSatisfies(scriptedFunction(3 to true)))
