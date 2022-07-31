7b3279ab9f5ed6f8556bd6ebbeea720f627c2fa9
Tom Yuval <tomuvak@gmail.com>
2022-07-31 16:48:07 +0200

    Nullables (refactor): rewrite T?.toOptional() using ?.let and ?:

--- src/commonMain/kotlin/Nullables.kt
+++ src/commonMain/kotlin/Nullables.kt
@@ -1,5 +1,8 @@
 package com.tomuvak.optional
 
+import com.tomuvak.optional.Optional.None
+import com.tomuvak.optional.Optional.Value
+
 fun <T> Optional<T>.orNull(): T? = switch(null) { it }
 
-fun <T : Any> T?.toOptional(): Optional<T> = if (this != null) Optional.Value(this) else Optional.None
+fun <T : Any> T?.toOptional(): Optional<T> = this?.let(::Value) ?: None
