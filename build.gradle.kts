367dae6e8bcc393309f381968de0fbfcff6db63f
Tom Yuval <tomuvak@gmail.com>
2022-09-05 18:19:58 +0200

    Start work on version 0.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.tomuvak.optional"
-version = "0.0.7"
+version = "0.0.8-SNAPSHOT"
 
 val localProperties = Properties()
 project.rootProject.file("local.properties").takeIf { it.canRead() }?.inputStream()?.let(localProperties::load)
